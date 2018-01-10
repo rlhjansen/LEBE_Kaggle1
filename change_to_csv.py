1099d32c10c0249482497a1d2a07c303770f1475
rlhjansen <rlh.jansen@outlook.com>
2018-01-10 14:41:44 +0100

    tsv naar csv converter gemaakt, als je de file "change_to_csv.py" runt zoals ie nu is dan converteer je de traindata naar csv, mits je traindata in dezelfde directory als je github map staat (dus 1 map boven deze file)

---
+++ change_to_csv.py
@@ -0,0 +1,22 @@
+import csv
+import os
+
+
+
+
+
+# example:
+# change_to_csv("test.tsv", "test.csv")
+# result = csv file van deze data
+def change_to_csv(filename, outname, enc="utf8"):
+    with open(os.path.join(os.pardir, filename), encoding=enc) as tsvin:
+        with open(os.path.join(os.pardir, outname), 'w', encoding=enc) as csvout:
+            tsvs = csv.reader(tsvin, delimiter='\t')
+            for row in tsvs:
+                newrow = ",".join(row) + "\n"
+                csvout.write(newrow)
+            csvout.close()
+        tsvin.close()
+
+
+change_to_csv("train.tsv", "train.csv")
